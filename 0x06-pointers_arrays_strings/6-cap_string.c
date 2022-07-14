508aaf48531067043c999908c35cf09cf7e46720
CynthiaMugo <cynthiamugo25@gmail.com>
2022-07-14 11:21:01 -0700

    Task 6 fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * cap_string -> capitalizes all words of a string
  * @str: string parameter
@@ -9,11 +8,11 @@ char *cap_string(char *str)
 {
 	int index = 0;
 
-	while (str[index])
+	while (str[++index])
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'Z'))
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
-		if (str[index - 1] == '' ||
+		if (str[index - 1] == ' ' ||
 				str[index - 1] == '\t' ||
 				str[index - 1] == '\n' ||
 				str[index - 1] == ',' ||
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,1 +1,2 @@
 This a readme file for more pointers, strings and arrays
+Expect the best. Prepare for the worst. Capitalize on what comes
