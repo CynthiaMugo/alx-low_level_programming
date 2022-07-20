d48d8607f65dc2c5bb8473a0b909292f2de2068e
CynthiaMugo <cynthiamugo25@gmail.com>
2022-07-20 12:35:19 -0700

    Task 8 fix

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -22,7 +22,7 @@ int substring_match(char *s1, char *s2, char *after_wldcd)
 	if (*s2 == '*')
 		return (substring_match(s1, s2 + 1, s2 + 1));
 
-	if (*s2 == *s2)
+	if (*s1 == *s2)
 		return (substring_match(s1 + 1, s2 + 1, after_wldcd));
 
 	else
