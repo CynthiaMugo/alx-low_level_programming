57ba66e2deb0c968cb4a9f7c1334bbfd22ac0272
CynthiaMugo <cynthiamugo25@gmail.com>
2022-08-01 11:02:38 -0700

    Task 2 corrections

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,8 @@ struct dog
 /**
  * dog_t -> a new name for the type struct dog
  */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
