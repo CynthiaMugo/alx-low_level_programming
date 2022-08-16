683d73aeaddd66dd74c3ee0f513c5416acc07e03
CynthiaMugo <cynthiamugo25@gmail.com>
2022-08-15 23:58:36 -0700

    no of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len -> counts the no of nodes in a linked list
+ * @h: head of the list
+ * Return: the no of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *cursor = h;
+	size_t count = 0;
+
+	while (cursor != NULL)
+	{
+		cursor += 1;
+		cursor = cursor->next;
+	}
+	return (count);
+}
